ced9c89e28e1472cb9371ba3c268dfc9a4eda8d8
Abhinand A K <abhinandak@outlook.com>
2021-12-28 19:51:28 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/AbhinandAK350/device_xiaomi_onclite.git -b pixys_twelve device/xiaomi/onclite
 git clone https://github.com/AbhinandAK350/vendor_xiaomi_onclite.git -b twelve vendor/xiaomi/onclite
-git clone https://github.com/AbhinandAK350/kernel_xiaomi_onclite.git -b eleven kernel/xiaomi/onclite
+git clone https://github.com/Dhina17/android_vendor_xiaomi_onclite.git -b lineage-18.1 kernel/xiaomi/onclite
