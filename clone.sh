33b614d9223a27105bf96310fd352d521faaaf7b
Abhinand A K <abhinandak@outlook.com>
2022-01-19 13:59:32 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/AbhinandAK350/device_xiaomi_onclite.git -b test device/xiaomi/onclite
-git clone https://github.com/PixysOS-Devices/vendor_xiaomi_onclite -b eleven vendor/xiaomi/onclite
-git clone https://github.com/PixysOS-Devices/kernel_xiaomi_onclite -b eleven kernel/xiaomi/onclite
+git clone https://github.com/AbhinandAK350/device_xiaomi_onclite.git -b pixys_twelve device/xiaomi/onclite
+git clone https://github.com/AbhinandAK350/vendor_xiaomi_onclite -b eleven vendor/xiaomi/onclite
+git clone https://github.com/AbhinandAK350/kernel_xiaomi_onclite -b twelve kernel/xiaomi/onclite
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-18.1 packages/resources/devicesettings
